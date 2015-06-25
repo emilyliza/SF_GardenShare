dcebc1348496d792c10cae9a1c9be538e6ed0f55
Emily Hittle <emilyliza@gmail.com>
2015-06-25 06:25:51 -0700

    heroku push

--- config/env/all.js
+++ config/env/all.js
@@ -32,13 +32,14 @@ module.exports = {
 			]
 		},
 		css: [
-			'public/modules/core/css/core.css'
+			'public/modules/**/css/*.css',
 
 		],
 		js: [
 			'public/config.js',
 			'public/application.js',
-			'public/modules/*/*.js',
+			'public/modules/**/*.js',
+			'public/modules/*/*.js'
 			'public/modules/*/*[!tests]*/*.js',
 
 		],
